f30840d9bf7bbd23f2c96976dff8280b28646745
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-10-09 09:04:48 -0300

    Update main.py
    função para encontrar a coordenada do clique

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import turtle
 tela = turtle.Screen()
 
 tela.title("U.S. States Game")
+image = "blank_states_img.gif"
+tela.addshape(image)
+turtle.shape(image)
+
+def local_clique(x, y):
+    print(x, y)
+turtle.onscreenclick(local_clique)
+turtle.mainloop()
 
 
 
-
-
-tela.exitonclick()
+#tela.exitonclick()
