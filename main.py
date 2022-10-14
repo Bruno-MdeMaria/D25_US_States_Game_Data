6d814a484d4a05ba9ac8f23049a80fcb19180790
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-10-14 00:01:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+import pandas
+
 
 tela = turtle.Screen()
 
@@ -7,14 +9,24 @@ image = "blank_states_img.gif"
 tela.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+todos_estados = data.state.to_list()
+print(todos_estados)
+
+
+resposta_estado = tela.textinput(title="Escolha um estado", prompt="Qual o nome do estado?: ")
+
+if resposta_estado == todos_estados:
+    t = turtle.Turtle() #cria uma turtle
+    t.hideturtle()    #esconde turtle
+    t.penup()
+    t.goto()
+
+
+
+tela.exitonclick()
 #FUNÇÃO PARA ENCONTRAR AS COORDENADAS DENTRO DO MAPA:
 # def local_clique(x, y):
 #     print(x, y)
 # turtle.onscreenclick(local_clique)
 # turtle.mainloop()
-
-resposta_estado = tela.textinput(title="Escolha um estado", prompt="Qual o nome do estado?: ")
-
-
-
-tela.exitonclick()
