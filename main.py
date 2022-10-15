80ed3799e9cd81667b061fd27512958b6bb7b8d3
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-10-15 08:55:31 -0300

    Update main.py
    Criado loop e descrição de acertos no painel.

--- main.py
+++ main.py
@@ -13,17 +13,21 @@ data = pandas.read_csv("50_states.csv")
 todos_estados = data.state.to_list()
 print(todos_estados)
 
+estados_adivinhados = []
 
-resposta_estado = tela.textinput(title="Escolha um estado", prompt="Qual o nome do estado?: ")
-print(resposta_estado)
+while len(estados_adivinhados) < 50: #enquanto a quantidade(len) de estados advinhados for menor que 50:
+    resposta_estado = tela.textinput(title=f"{len(estados_adivinhados)}/50 State Correct", prompt="Qual o nome do estado?: ")
+    print(resposta_estado)
 
-if resposta_estado in todos_estados:
-    t = turtle.Turtle() #cria uma turtle
-    t.hideturtle()    #esconde turtle
-    t.penup()
-    state_data = data[data.state == resposta_estado] #se o estado escolhido for igual a algum estado na tabela
-    t.goto(int(state_data.x),int(state_data.y)) #como a variavel receberá a linha da tabela podemos chamar os valores x e y diretamente usando a descrição da coluna.
-    t.write(resposta_estado)  #método de escrita da turtle irá escrever a escolha no local correto.
+
+    if resposta_estado in todos_estados:   #se a resposta estiver dentro da planilha:
+        estados_adivinhados.append(resposta_estado)
+        t = turtle.Turtle() #cria uma turtle
+        t.hideturtle()    #esconde turtle
+        t.penup()
+        state_data = data[data.state == resposta_estado] #se o estado escolhido for igual a algum estado na tabela
+        t.goto(int(state_data.x),int(state_data.y)) #como a variavel receberá a linha da tabela podemos chamar os valores x e y diretamente usando a descrição da coluna.
+        t.write(resposta_estado)  #método de escrita da turtle irá escrever a escolha no local correto.
 
 
 
